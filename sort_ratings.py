063ef0aa35e17bac6c9292aefd75559aad06d52e
Mohamed Jama <mohamed.jama@ucdconnect.ie>
2021-12-10 11:10:20 +0000

    refactor header ignore mechanism.
    Start from the second row, rather then create unnecessary index counter manipulation.

--- sort_ratings.py
+++ sort_ratings.py
@@ -5,24 +5,18 @@ def begin_system():
 	# Type: users -> users[userID][itemID] = rating for item.
 	with open('ratings.csv') as csvFile:
 		readCSVFile = csv.reader(csvFile, delimiter=',')
-		index = 0
-		for row in readCSVFile:
-			if index!=0:
-				if row[0] not in users:
-					users[row[0]] = {}
-					users[row[0]][row[1]] = float(row[2])
-				else:
-					users[row[0]][row[1]] = float(row[2])
-			index+=1
+		for row in readCSVFile[1:]:
+			if row[0] not in users:
+				users[row[0]] = {}
+				users[row[0]][row[1]] = float(row[2])
+			else:
+				users[row[0]][row[1]] = float(row[2])
 
 	movie_list = []
 	with open('movies.csv', encoding="utf8") as csvFile:
 		readCSVFile = csv.reader(csvFile, delimiter=',')
-		index = 0
-		for row in readCSVFile:
-			if index!=0:
-				movie = []
-				movie = [row[0], row[1]]
-				movie_list.append(movie)
-			index+=1
+		for row in readCSVFile[1:]:
+			movie = []
+			movie = [row[0], row[1]]
+			movie_list.append(movie)
 	return [users,movie_list]
